0f303af7f99e24a82a6722f55e0cfa75583df8e0
Nagendra T P <nagendraputhane@gmail.com>
2025-07-21 11:15:48 +0530

    conf.py syntax errors

--- doc/guides/conf.py
+++ doc/guides/conf.py
@@ -39,8 +39,6 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
@@ -56,7 +54,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata-sphinx-theme'
+html_theme = 'pydata_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -68,25 +66,25 @@ html_static_path = ['_static']
 # or fully qualified paths (eg. https://...)
 
 branch = os.environ.get("GITHUB_REF_NAME", "local")
-if branch.startswith("dao-"):
-    release= branch.replace("dao-", "")
-elif branch == "dao-devel":
+if branch.startswith("docs-v"):
+    release= branch.replace("docs-v", "")
+elif branch == "docs":
     release = "latest"
 else:
     release = branch
  
 html_theme_options = {
-    "navbar_end": ["version-switcher", navbar-icon-links"],
+    "navbar_end": ["version-switcher", "navbar-icon-links"],
     "switcher": {
         "json_url": "https://marvellembeddedprocessors.github.io/dao/versions.json",
         "version_match": release,
     },
 }
  
-extensions = [
-    "sphinx_multiversion",
-]
+#extensions = [
+#    "sphinx_multiversion",
+#]
  
-smv_branch_whitelist = r"^dao-(\d{2}\.\d{2})$|^dao-devel$"
-smv_tag_whitelist = r""
-smv_outputdir_format = "{ref.name}"
+#smv_branch_whitelist = r"^dao-(\d{2}\.\d{2})$|^dao-devel$"
+#smv_tag_whitelist = r""
+#smv_outputdir_format = "{ref.name}"
