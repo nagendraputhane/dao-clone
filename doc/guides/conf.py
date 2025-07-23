7006009ddd17010966460a7f757cd09ababb1289
Nagendra T P <nagendraputhane@gmail.com>
2025-07-23 08:40:32 +0530

    back to normal

--- doc/guides/conf.py
+++ doc/guides/conf.py
@@ -20,10 +20,6 @@
 
 # -- Project information -----------------------------------------------------
 
-from datetime import datetime
-import os
-import re
-
 project = 'Data Accelerator Offload'
 copyright = '2024, Marvell'
 author = 'Marvell'
@@ -39,6 +35,8 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
@@ -54,7 +52,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -64,27 +62,11 @@ html_static_path = ['_static']
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
-
-branch = os.environ.get("GITHUB_REF_NAME", "local")
-if branch.startswith("docs-v"):
-    release= branch.replace("docs-v", "")
-elif branch == "docs":
-    release = "latest"
-else:
-    release = branch
- 
-html_theme_options = {
-    "navbar_end": ["version-switcher", "navbar-icon-links"],
-    "switcher": {
-        "json_url": "https://marvellembeddedprocessors.github.io/dao/versions.json",
-        "version_match": release,
-    },
-}
- 
-extensions = [
-    "sphinx_multiversion",
+html_css_files = [
+    'css/custom.css',
 ]
- 
-#smv_branch_whitelist = r"^dao-(\d{2}\.\d{2})$|^dao-devel$"
-#smv_tag_whitelist = r""
-#smv_outputdir_format = "{ref.name}"
+html_js_files = [
+    'js/titleprefix.js',
+]
+
+html_title = ""
