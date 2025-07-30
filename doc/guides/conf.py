15ebb3e5b5621105579707b0f515d8d4963be85a
Nagendra T P <nagendraputhane@gmail.com>
2025-07-30 10:28:03 +0530

    enhancement v1
    - Update Readme
    - GitHub Link
    - Copyright dynamic year
    - remove This Page / Show Source
    - Wrap around NavBar
    - Small search button
    - New DAO logo
    - Enlarge image. Enlarge top NavBar to fit image.
    - Tab Icon
    - Copy button for code
    
    Signed-off-by: Nagendra T P <nagendraputhane@gmail.com>

--- doc/guides/conf.py
+++ doc/guides/conf.py
@@ -1,92 +1,67 @@
 # SPDX-License-Identifier: Marvell-MIT
-# Copyright (c) 2024 Marvell.
-#
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Main Sphinx configuration for DAO docs
+# ---------------------------------------------------------------------------
 
-# -- Path setup --------------------------------------------------------------
+from datetime import datetime
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-# sys.path.insert(0, os.path.abspath('.'))
+project   = "Data Accelerator Offload"
+author    = "Marvell"
+release   = "25.05.0"
+copyright = f"2024-{datetime.now().year}, Marvell"
+html_logo = "logo/dao_logo.png"
+master_doc = "index"
 
+# ---------------------------------------------------------------------------
+extensions = ["sphinx_copybutton", "sphinx_multiversion", "sphinx_design"]
 
-# -- Project information -----------------------------------------------------
+smv_branch_whitelist = r"^dao-.*|^dao-devel$"
+smv_tag_whitelist    = r"^$"
+smv_remote_whitelist = r"^origin$"
+smv_outputdir_format = "{ref.name}"
 
-project = 'Data Accelerator Offload'
-copyright = '2024, Marvell'
-author = 'Marvell'
-html_logo = 'logo/logo.jpg'
-# The full version, including alpha/beta/rc tags
-release = '0.0.1'
+# ---------------------------------------------------------------------------
+html_theme = "pydata_sphinx_theme"
+version    = "dao-devel"                           # this branch’s label
 
-# The master toctree document.
-master_doc = 'index'
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-]
-
-extensions += ["sphinx_multiversion"]
-
-smv_branch_whitelist = r'^dao-.*|^dao-devel$'
-smv_tag_whitelist = r'^$'
-smv_remote_whitelist = r'^origin$'
-
-# Add any paths that contain templates here, relative to this directory.
-#templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'pydata_sphinx_theme'
-
-version = "dao-devel"
-
-html_context = {
-    "version": version,
-}
+html_context = {"version": version}
 
 html_theme_options = {
     "switcher": {
         "json_url": "https://nagendraputhane.github.io/dao-clone/versions.json",
         "version_match": version,
     },
-    "navbar_end": ["version-switcher", "theme-switcher"],
+    "external_links": [
+        {"name": "Home", "url": "./"},
+    ],
+    "icon_links": [
+        {
+            "name": "GitHub Repo",
+            "url": "https://github.com/nagendraputhane/dao-clone",
+            "icon": "fa-brands fa-github",
+            "type": "fontawesome",
+        },
+    ],
+    "default_mode": "light",                       # light-only
+    "navbar_end": ["search-button",
+                   "version-switcher",
+                   "navbar-icon-links"],
 }
 
+html_meta = {
+    "color-scheme": "light",
+}
+
+html_static_path   = ["_static"]
+html_css_files     = ["css/custom.css"]
+html_js_files      = ["js/titleprefix.js"]
+html_favicon       = "_static/tab_logo.jpg"
+html_show_sourcelink = False
 
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# These folders are copied to the documentation's HTML output
-html_static_path = ['_static']
+html_sidebars = {
+    "index": [],
+}
 
-# These paths are either relative to html_static_path
-# or fully qualified paths (eg. https://...)
+exclude_patterns   = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_js_files = [
-    'js/titleprefix.js',
-]
-
-html_title = ""
+templates_path = ["_templates"]
--- doc/guides/index.rst
+++ doc/guides/index.rst
@@ -1,18 +1,85 @@
 ..  SPDX-License-Identifier: Marvell-MIT
     Copyright (c) 2024 Marvell.
 
-Welcome to Data Accelerator Offload(DAO) Documentation
-======================================================
+Data Accelerator Offload (DAO) Documentation
+============================================
 
 .. toctree::
- :maxdepth: 2
+   :maxdepth: 1
+   :hidden:
 
- intro
- gsg/index
- prog_guide/index
- applications/index
- howtoguides/index
- contributing/index
- rel_notes/index
- community_lab/index
- faq/index
+   intro
+   guides
+   resources
+   community_lab/index
+
+.. grid:: 1 1 2 2
+   :gutter: 2 3 4 4
+
+   .. grid-item-card::
+      :text-align: center
+
+      **Introduction**
+      ^^^
+
+      What DAO is, architecture, and how these docs are organized.
+
+      +++
+
+      .. button-ref:: intro
+         :color: secondary
+         :expand:
+         :click-parent:
+
+         Read the introduction
+
+   .. grid-item-card::
+      :text-align: center
+
+      **Developer Guides**
+      ^^^
+
+      Install, program, and build with DAO: platform, libraries, apps, and how-tos.
+
+      +++
+
+      .. button-ref:: guides
+         :color: secondary
+         :expand:
+         :click-parent:
+
+         Browse the guides
+
+   .. grid-item-card::
+      :text-align: center
+
+      **Resources**
+      ^^^
+
+      Contributing, release notes, and FAQs.
+
+      +++
+
+      .. button-ref:: resources
+         :color: secondary
+         :expand:
+         :click-parent:
+
+         Open the resources hub
+
+   .. grid-item-card::
+      :text-align: center
+
+      **Community Lab**
+      ^^^
+
+      Hands-on labs to try DAO features.
+
+      +++
+
+      .. button-ref:: community_lab/index
+         :color: secondary
+         :expand:
+         :click-parent:
+
+         Launch the Community Lab
